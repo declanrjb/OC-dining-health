578f965b2691b847ce5953f05d846dfebd0ec492
declanrjb <declanrjb@gmail.com>
2025-04-13 14:33:36 -0400

    standardize stevie reports

--- notes.md
+++ notes.md
@@ -7,3 +7,6 @@ lord saunders: https://healthspace.com/Clients/Ohio/LorainCounty/Web.nsf/Food-Fa
 wilder: https://healthspace.com/Clients/Ohio/LorainCounty/Web.nsf/Food-FacilityHistory?OpenView&RestrictToCategory=9AFBD4E8FC2F72E888257BF6007CF58B
 
 heritage: https://healthspace.com/Clients/Ohio/LorainCounty/Web.nsf/Food-FacilityHistory?OpenView&RestrictToCategory=467771A0DB5AD6D3852580BC00634686
+
+stevie had a critical violation on wednesday for live pests
+wilder also had one critical violation for sanitization temperature
--- scrape.R
+++ scrape.R
@@ -11,6 +11,32 @@ filter_vec <- function(vec, condition) {
     return()
 } 
 
+get_critical <- function(page) {
+  text <- page %>%
+    html_nodes('#general') %>%
+    html_text()
+  if (length(text) > 0) {
+    return(text)
+  } else {
+    return('')
+  }
+}
+
+get_noncritical <- function(page) {
+  text <- page %>%
+    html_nodes('#manager') %>%
+    html_text()
+  if (length(text) > 0) {
+    return(text)
+  } else {
+    return('')
+  }
+}
+
+url <- 'https://healthspace.com/Clients/Ohio/LorainCounty/Web.nsf/Food-FacilityHistory?OpenView&RestrictToCategory=5A9F8BE97B8078C388257BF6007CF58C'
+
+page <- read_html(url)
+
 df <- page %>% 
   html_nodes('table') %>% 
   .[5] %>% 
@@ -24,3 +50,19 @@ df$link <- page %>%
   html_attr('href') %>%
   filter_vec(function(link) {grepl('Food-Inspection', link)}) %>%
   paste('https://healthspace.com/Clients/Ohio/LorainCounty/Web.nsf/', ., sep='')
+
+pages <- df$link %>%
+  lapply(read_html)
+
+df$critical <- pages %>%
+  lapply(get_critical) %>%
+  unlist() %>%
+  gsub('\t', '', .)
+
+df$non_critical <- pages %>%
+  lapply(get_noncritical) %>%
+  unlist() %>%
+  gsub('\t', '', .)
+
+df %>%
+  write.csv('data/stevie.csv', row.names=FALSE)
